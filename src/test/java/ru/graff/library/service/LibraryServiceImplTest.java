4792114ca9285b2c1ac1bc9f4a74f4a85d09a111
graff913 <graff913@outlook.com>
2019-08-10 17:28:39 +0300

    Исправил замечания по тестам

--- src/test/java/ru/graff/library/service/LibraryServiceImplTest.java
+++ src/test/java/ru/graff/library/service/LibraryServiceImplTest.java
@@ -2,12 +2,11 @@ package ru.graff.library.service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
 import ru.graff.library.domain.Author;
 import ru.graff.library.domain.Book;
 import ru.graff.library.domain.Style;
@@ -15,33 +14,38 @@ import ru.graff.library.repository.AuthorRepository;
 import ru.graff.library.repository.BookRepository;
 import ru.graff.library.repository.StyleRepository;
 
+import java.util.Collections;
 import java.util.Optional;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.doReturn;
 
 @RunWith(SpringRunner.class)
-@DataJpaTest
-@Transactional
+@SpringBootTest("spring.shell.interactive.enabled=false")
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class LibraryServiceImplTest {
 
-    @Autowired
+    @MockBean
     private BookRepository bookRepository;
 
-    @Autowired
+    @MockBean
     private StyleRepository styleRepository;
 
-    @Autowired
+    @MockBean
     private AuthorRepository authorRepository;
 
     @Test
     public void showAll() {
         Author author = new Author("Тестовый автор");
-        authorRepository.save(author);
         Style style = new Style("Тестовый стиль");
-        styleRepository.save(style);
         Book book = new Book("Тестовая книг", author, style);
-        bookRepository.save(book);
+
+        doReturn(1L).when(authorRepository).count();
+        doReturn(1L).when(styleRepository).count();
+        doReturn(1L).when(bookRepository).count();
+        doReturn(Collections.singletonList(author)).when(authorRepository).findAll();
+        doReturn(Collections.singletonList(style)).when(styleRepository).findAll();
+        doReturn(Collections.singletonList(book)).when(bookRepository).findAll();
 
         LibraryService libraryService = new LibraryServiceImpl(bookRepository, authorRepository, styleRepository);
 
@@ -55,12 +59,23 @@ public class LibraryServiceImplTest {
 
     @Test
     public void workWithBook() {
+        Optional<Author> mockAuthor = Optional.of(new Author("Тестовый автор"));
+        Style mockStyle = new Style("Тестовый стиль");
+        Book mockBook = new Book("Тестовая книг", mockAuthor.get(), mockStyle);
+
+        doReturn(mockAuthor).when(authorRepository).findByName("Тестовый автор");
+        doReturn(mockStyle).when(styleRepository).findByName("Тестовый стиль");
+        doReturn(Collections.singletonList(mockBook)).when(bookRepository).findAll();
+
         LibraryService libraryService = new LibraryServiceImpl(bookRepository, authorRepository, styleRepository);
         libraryService.addBook("Тестовая книг", "Тестовый автор", "Тестовый стиль");
+
         Book book = libraryService.showAllBooks().get(0);
         assertEquals("Тестовая книг", book.getName());
         assertEquals("Тестовый автор", book.getAuthor().getName());
         assertEquals("Тестовый стиль", book.getStyle().getName());
+
+        doReturn(Optional.of(mockBook)).when(bookRepository).findById(0);
 
         libraryService.updateBook(book.getId(), "Тестовая книг 2" , "Тестовый автор 2", "Тестовый стиль 2");
 
@@ -68,8 +83,6 @@ public class LibraryServiceImplTest {
         assertEquals("Тестовая книг 2", updateBook.get().getName());
         assertEquals("Тестовый автор 2", updateBook.get().getAuthor().getName());
         assertEquals("Тестовый стиль 2", updateBook.get().getStyle().getName());
-
-
     }
 
 }
